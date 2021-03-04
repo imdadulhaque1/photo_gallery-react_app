1273c1dc8991e15e120b2070afc76802157f369e
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-03-05 01:35:01 +0600

    Created Components and all the needed pages !

---
+++ src/components/PhotoGallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PhotoGallery = () => {
+    return (
+        <div>
+            <h1>This is photo gallery !</h1>
+        </div>
+    )
+}
+
+export default PhotoGallery;
