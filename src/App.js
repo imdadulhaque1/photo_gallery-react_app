f34f9233ea792857a17c61b1c4e8105adb1a1b11
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-03-05 02:43:40 +0600

    Navbar Created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Navbar from "./components/Header/Navbar";
+import Home from "./components/Home";
+import PhotoGallery from "./components/PhotoGallery";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
-    <div>
-      <h1>Hi this is photo-gallery !</h1>
-    </div>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/photogallery" component={PhotoGallery} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/footer" component={Footer} />
+    </Switch>
     </>
   );
 }
