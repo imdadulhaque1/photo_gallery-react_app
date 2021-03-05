d3d05c7c89afc5cd49406848714595dd7d32ad27
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-03-05 22:40:33 +0600

    Navbar Created !

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -6,19 +6,24 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">I.H.-PhotoGallery</a>
+                    <NavLink className="navbar-brand" to="/">I.H.-PhotoGallery</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <NavLink className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </NavLink>
-                        <NavLink className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                        </NavLink>
-                        
+                        <li className="nav-item">
+                        <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                        <NavLink className="nav-link" to="/photogallery">Photo Gallery</NavLink>
+                        </li>
+                        <li className="nav-item">
+                        <NavLink className="nav-link" to="/about">About</NavLink>
+                        </li>
+                        <li className="nav-item">
+                        <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                        </li>
                     </ul>
                     </div>
                 </div>
