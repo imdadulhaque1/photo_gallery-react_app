bf5d60316ad4d687ee0c81c8800cdc0928cd9709
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-03-06 01:14:46 +0600

    Photo Gallery Created now need to customized

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -16,16 +16,16 @@ const Navbar = () => {
                             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                                <NavLink activeClassName="menu_active" className="nav-link" aria-current="page" to="/">Home</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                <NavLink className="nav-link" to="/photogallery">Photo Gallery</NavLink>
+                                <NavLink activeClassName="menu_active" className="nav-link" to="/photogallery">Photo Gallery</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                <NavLink className="nav-link" to="/about">About</NavLink>
+                                <NavLink activeClassName="menu_active" className="nav-link" to="/about">About</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                                <NavLink activeClassName="menu_active" className="nav-link" to="/contact">Contact</NavLink>
                                 </li>
                             </ul>
                             </div>
--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -3,6 +3,9 @@ import { NavLink } from "react-router-dom";
 import OnlineAchievement from "../images/OnlineAchieveMent/NLP.png";
 import Tour from "../images/TourTime/IMG_20200907_131820.jpg";
 import CampusTime from "../images/CampusTime/IMG_20200928_191732.jpg";
+import Publication from "../images/Publication/python crush course.png";
+import Eating from "../images/Eatting/IMG_20210211_204943.jpg";
+import DIU_Convocation from "../images/DIU_Convocation/5.jpg";
 const PhotoGallery = () =>{
   return(
     <>
@@ -15,35 +18,82 @@ const PhotoGallery = () =>{
            <div className="row gy-4">
 
             <div className="col-md-4 col-10 mx-auto">
-                    <div className="card">
-                    <img src={OnlineAchievement} className="card-img-top" alt="Online achievement" />
-                        <div className="card-body">
-                        <h5 className="card-title font-weight-bold">Online Achievement</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <NavLink to="" className="btn btn-primary">
+                <div className="card">
+                <img src={OnlineAchievement} className="card-img-top" alt="Online achievement" />
+                <div className="card-body">
+                    <h5 className="card-title font-weight-bold">Online Achievement</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <NavLink to="" className="btn btn-primary">
                             Show Album
-                        </NavLink>
-                        </div>
-
-                        <img src={Tour} className="card-img-top" alt="Tour Time" />
-                        <div className="card-body">
-                        <h5 className="card-title font-weight-bold">Tour Time</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <NavLink to="" className="btn btn-primary">
-                            Show Album
-                        </NavLink>
-                        </div>
-
-                        <img src={CampusTime} className="card-img-top" alt="CAmpus Time" />
-                        <div className="card-body">
-                        <h5 className="card-title font-weight-bold">Campus Time</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <NavLink to="" className="btn btn-primary">
-                            Show Album
-                        </NavLink>
-                        </div>
-                    </div>
+                    </NavLink>
                 </div>
+                </div>
+            </div>
+
+            <div className="col-md-4 col-10 mx-auto">
+                <div className="card">
+                <img src={Publication} className="card-img-top" alt="Publication" />
+                <div className="card-body">
+                    <h5 className="card-title font-weight-bold">Publication</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <NavLink to="" className="btn btn-primary">
+                            Show Album
+                    </NavLink>
+                </div>
+                </div>
+            </div>
+
+            <div className="col-md-4 col-10 mx-auto">
+                <div className="card">
+                <img src={CampusTime} className="card-img-top" alt="Campus Time" />
+                <div className="card-body">
+                    <h5 className="card-title font-weight-bold">Campus Time</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <NavLink to="" className="btn btn-primary">
+                            Show Album
+                    </NavLink>
+                </div>
+                </div>
+            </div>
+
+            <div className="col-md-4 col-10 mx-auto">
+                <div className="card">
+                <img src={Eating} className="card-img-top" alt="Eating" />
+                <div className="card-body">
+                    <h5 className="card-title font-weight-bold">Eating</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <NavLink to="" className="btn btn-primary">
+                            Show Album
+                    </NavLink>
+                </div>
+                </div>
+            </div>
+
+            <div className="col-md-4 col-10 mx-auto">
+                <div className="card">
+                <img src={DIU_Convocation} className="card-img-top" alt="DIU Convocation" />
+                <div className="card-body">
+                    <h5 className="card-title font-weight-bold">DIU Convocation</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <NavLink to="" className="btn btn-primary">
+                            Show Album
+                    </NavLink>
+                </div>
+                </div>
+            </div>
+
+            <div className="col-md-4 col-10 mx-auto">
+                <div className="card">
+                <img src={Tour} className="card-img-top" alt="Tour" />
+                <div className="card-body">
+                    <h5 className="card-title font-weight-bold">Tour</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <NavLink to="" className="btn btn-primary">
+                            Show Album
+                    </NavLink>
+                </div>
+                </div>
+            </div>
 
            </div>
          </div>
