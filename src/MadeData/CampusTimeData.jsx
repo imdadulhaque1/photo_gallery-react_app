9715220d5d53f141a4b1b250748402c10c88f7e2
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-03-07 01:44:19 +0600

    Made the own data of PublicationData and CampusTimedata

---
+++ src/MadeData/CampusTimeData.jsx
@@ -0,0 +1,35 @@
+import semester1 from "../images/CampusTime/1.jpg";
+import semester2 from "../images/CampusTime/2.jpg";
+import semester3 from "../images/CampusTime/3.jpg";
+import semester4 from "../images/CampusTime/4.jpg";
+import semester9 from "../images/CampusTime/9.jpg";
+import semester12 from "../images/CampusTime/12.jpg";
+
+const CampusTimeData =[
+    {
+        imgsrc: semester1,
+        title: "First Semester",
+    },
+    {
+        imgsrc: semester2,
+        title: "Second Semester",
+    },
+    {
+        imgsrc: semester3,
+        title: "Third Semester",
+    },
+    {
+        imgsrc: semester4,
+        title: "Four Semester",
+    },
+    {
+        imgsrc: semester9,
+        title: "Nine Semester",
+    },
+    {
+        imgsrc: semester12,
+        title: "Twelve Semester",
+    },
+];
+
+export default CampusTimeData;
---
+++ src/MadeData/PublicationData.jsx
@@ -0,0 +1,20 @@
+import acceptness1 from "../images/Publication/acceptness_1.png";
+import presentation1 from "../images/Publication/presentation_1.jpg";
+import acceptness2 from "../images/Publication/m_acc_1.png";
+
+const PublicationData =[
+    {
+        imgsrc: acceptness1,
+        title: "Accept Letter of Deep Learning based Classification of Papaya Disease Recognition",
+    },
+    {
+        imgsrc: presentation1,
+        title: "Presentation Certificate of Deep Learning based Classification of Papaya Disease Recognition",
+    },
+    {
+        imgsrc: acceptness2,
+        title: "Malabar Nightshde Disease Detection Using Deep Learning Techniques",
+    },
+];
+
+export default PublicationData;
