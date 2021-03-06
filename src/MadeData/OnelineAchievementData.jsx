adc905d62a60cafe7cc1df9da14c0d16741cb5f0
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-03-06 23:06:09 +0600

    Working for Oneline Achievement Pages to link with Photo Gallery ablum Online achievement

---
+++ src/MadeData/OnelineAchievementData.jsx
@@ -0,0 +1,35 @@
+import Python from "../images/OnlineAchieveMent/python crush course.png";
+import Tensorflow from "../images/OnlineAchieveMent/Intro to Tensorflow for AI_ML_DL.png";
+import NLP from "../images/OnlineAchieveMent/NLP.png";
+import HtmlCss from "../images/OnlineAchieveMent/HYML CSS.png";
+import DjangoBackend from "../images/OnlineAchieveMent/Djanggo Backend.png";
+import DjangoProject from "../images/OnlineAchieveMent/Djanggo PROJECT.png";
+
+const OnelineAchievementData =[
+    {
+        imgsrc: Python,
+        title: "Python Crush Course",
+    },
+    {
+        imgsrc: Tensorflow,
+        title: "Intro to Tensorflow for AI ML DL",
+    },
+    {
+        imgsrc: NLP,
+        title: "Natural Language Processing",
+    },
+    {
+        imgsrc: HtmlCss,
+        title: "HTML CSS for Web Designing",
+    },
+    {
+        imgsrc: DjangoBackend,
+        title: "Django Backend for Developing",
+    },
+    {
+        imgsrc: DjangoProject,
+        title: "Djnago Projects",
+    },
+];
+
+export default OnelineAchievementData;
--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -23,8 +23,8 @@ const PhotoGallery = () =>{
                 <div className="card-body">
                     <h5 className="card-title font-weight-bold">Online Achievement</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <NavLink to="" className="btn btn-primary">
-                            Show Album
+                    <NavLink to="/src/components/Gallery/OnelineAchievementPage.jsx" className="btn btn-primary">
+                           <button> Show Album</button>
                     </NavLink>
                 </div>
                 </div>
